d0ecefca9149ced77236f5f03b1469654c7ecea2
Muhammet Cin <muhammetacin@gmail.com>
2022-03-17 10:48:34 +0100

    Change title of all pages

--- Starter-pack/overview.php
+++ Starter-pack/overview.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Goodcard - track your collection of Pokémon cards</title>
+    <title>The Pokèmon Database</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -16,7 +16,7 @@
 </head>
 <body>
 
-<h1 class="d-flex justify-content-center text-white bg-danger py-3 mb-3">Goodcard - track your collection of Pokémon cards</h1>
+<h1 class="d-flex justify-content-center text-white bg-danger py-3 mb-3">The Pokèmon Database</h1>
 
 <div class="container">
     <table class="table">
