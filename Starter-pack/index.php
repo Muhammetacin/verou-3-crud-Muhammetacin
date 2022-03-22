e1c03acc41e8e21593f299e70577e219caed2a3d
Muhammet Cin <muhammetacin@gmail.com>
2022-03-22 11:03:31 +0100

    Comment config.php load

--- Starter-pack/index.php
+++ Starter-pack/index.php
@@ -9,7 +9,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // Load you classes
-require_once 'config.php';
+//require_once 'config.php';
 require_once 'classes/DatabaseManager.php';
 require_once 'classes/CardRepository.php';
 
